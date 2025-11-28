89b6c39a4a6ceeeb928d9c8cae5c19491f023301
I-like-penguins <ThomasRe@mailbox.org>
2025-11-28 19:41:50 +0100

    Re-did some more aspects mentioned by codio mentor

--- web_generator.py
+++ web_generator.py
@@ -14,7 +14,7 @@ def serialize_movie(movie) -> str:
     output_string += f'        <li>\n'
     output_string += f'            <div class="movie">\n'
     output_string += f'                <img class="movie-poster"\n'
-    output_string += f'                     src="{movie["poster_url"]} alt="{movie["title"]}">\n'
+    output_string += f'                     src="{movie["poster_url"]}" alt="{movie["title"]}">\n'
     output_string += f'                <div class="movie-title">{movie["title"]}</div>\n'
     output_string += f'                <div class="movie-year">{movie["year"]}</div>\n'
     output_string += f'            </div>\n'
